8d8552d3592263155ef8150a03344cb4538bffaa
Mo Gorhom <gorhom@me.com>
2020-01-13 23:16:21 +0100

    update reactnative example

--- packages/native/App.js
+++ packages/native/App.js
@@ -1,22 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {useEffect} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
-  Button,
+  TouchableOpacity,
 } from 'react-native';
 import {useCounter} from '@demo/shared';
+
+const Button = ({text = '', onPress}) => {
+  return (
+    <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
+      <Text style={styles.buttonText}>{text.toUpperCase()}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const App = () => {
   const {counter, increment, decrement} = useCounter();
@@ -24,11 +23,16 @@ const App = () => {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.center}>
-          <Text style={styles.text}>{counter}</Text>
-          <Button onPress={increment} title={'increment'} />
-          <Button onPress={decrement} title={'decrement'} />
+      <SafeAreaView style={styles.root}>
+        <View style={styles.container}>
+          <Text style={styles.title}>
+            React<Text style={styles.subtitle}>Monorepo</Text>
+          </Text>
+          <View style={styles.counterContainer}>
+            <Text style={styles.counterText}>{counter}</Text>
+          </View>
+          <Button onPress={increment} text={'increment'} />
+          <Button onPress={decrement} text={'decrement'} />
         </View>
       </SafeAreaView>
     </>
@@ -36,19 +40,46 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
     backgroundColor: 'black',
   },
-  center: {
+  container: {
+    display: 'flex',
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
+    padding: 24,
   },
-  text: {
-    fontSize: 24,
+  title: {
+    fontSize: 36,
+    fontWeight: '400',
+    color: '#fff',
+  },
+  subtitle: {
+    fontWeight: '600',
+    color: '#61dafb',
+  },
+  counterContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexGrow: 1,
+    alignItems: 'center',
+  },
+  counterText: {
+    fontSize: 64,
+    color: '#fff',
+  },
+  buttonContainer: {
+    backgroundColor: '#444',
+    padding: 12,
+    margin: 6,
+    minWidth: 200,
+    borderRadius: 23,
+  },
+  buttonText: {
+    fontSize: 18,
+    textAlign: 'center',
     color: '#fff',
   },
 });
--- packages/native/index.js
+++ packages/native/index.js
@@ -5,24 +5,4 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 
-const modules = require.getModules();
-const moduleIds = Object.keys(modules);
-const loadedModuleNames = moduleIds
-  .filter(moduleId => modules[moduleId].isInitialized)
-  .map(moduleId => modules[moduleId].verboseName);
-const waitingModuleNames = moduleIds
-  .filter(moduleId => !modules[moduleId].isInitialized)
-  .map(moduleId => modules[moduleId].verboseName);
-
-// make sure that the modules you expect to be waiting are actually waiting
-console.log(
-  'loaded:',
-  loadedModuleNames.length,
-  'waiting:',
-  waitingModuleNames.length
-);
-
-// grab this text blob, and put it in a file named packager/modulePaths.js
-console.log(`module.exports = ${JSON.stringify(loadedModuleNames.sort())};`);
-
 AppRegistry.registerComponent("App", () => App);
